e4a6b4aed27da7f7c548e70b630816f95f861089
aakash-vidhate <64070421+aakash-vidhate@users.noreply.github.com>
2020-05-23 21:58:26 +0530

    Add files via upload

---
+++ Assignment05/src/public/signup/signup.controller.js
@@ -0,0 +1,42 @@
+(function () {
+"use strict";
+angular.module('public')
+.controller('RegistrationController', RegistrationController);
+
+
+RegistrationController.$inject = ['RegistrationService'];
+function RegistrationController(RegistrationService) {
+  var reg = this;
+  var items=[];
+reg.wantitems=RegistrationService.getItems();
+        reg.user = {};
+        reg.favoriteDish = {};
+        reg.showError = false;
+        reg.showMessage = false;
+   //reg.items = RegistrationService.getItems();
+
+  reg.submit = function (fname,lname,email,phno,short_name) {
+    reg.completed = true;
+    reg.showError = false;
+    reg.showMessage = false;
+    //reg.user.items=RegistrationService.testItems(fname,lname,email,phno,short_name);
+    RegistrationService.testItems(fname,lname,email,phno,short_name).then(function(response) {
+               reg.user.favoriteDishDetails = response.data;
+               console.log(reg.favoriteDish);
+               RegistrationService.savedet(reg.user);
+               reg.showMessage = true;
+               reg.completed = true;
+           }, function(error) {
+               console.log(error);
+               reg.showError = true;
+           });
+
+  };
+
+
+
+}
+
+
+
+})();
